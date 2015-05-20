ca576fde0d91c75ee7c2e91a67877479f9511d13
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-20 17:01:16 +0900

    Add input plugin of github-activities

---
+++ lib/fluent/plugin/in_github_activities.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of fluent-plugin-github-activities.
+#
+# fluent-plugin-github-activities is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# fluent-plugin-github-activities is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with fluent-plugin-github-activities.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+module Fluent
+  class GithubActivitiesInput < Input
+    Plugin.register_input("github-activities", self)
+
+    def initialize
+      super
+    end
+
+    def start
+    end
+
+    def shutdown
+    end
+  end
+end
