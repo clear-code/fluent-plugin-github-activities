4a9063098586011a755192eb38c19eb1082a9c74
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-21 17:32:29 +0900

    Add sample code to try the crwaler

---
+++ sample/try_crawl.rb
@@ -0,0 +1,13 @@
+require "fluent/plugin/github-activities"
+
+crawler = Fluent::GithubActivities::Crawler.new
+crawler.on_emit = lambda do |tag, record|
+  puts "EMIT: #{tag}"
+end
+crawler.reserve_user_events("piroor")
+crawler.request_queue
+crawler.process_request
+crawler.request_queue.size
+(crawler.request_queue.size - 1).times do crawler.request_queue.shift end
+crawler.process_request
+crawler.request_queue
