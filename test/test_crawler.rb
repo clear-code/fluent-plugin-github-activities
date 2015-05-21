31a63029d5860f0333ca508753d7efafef65e081
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-21 14:52:40 +0900

    Add test to process push event with multiple commits

--- test/test_crawler.rb
+++ test/test_crawler.rb
@@ -17,6 +17,8 @@
 # License along with fluent-plugin-github-activities.  If not, see
 # <http://www.gnu.org/licenses/>.
 
+require "json"
+
 require "fluent/plugin/github-activities"
 
 class CrawlerTest < Test::Unit::TestCase
@@ -70,6 +72,35 @@ class CrawlerTest < Test::Unit::TestCase
         :emitted_records => [
           { :tag    => "test",
             :record => { "type" => "test" } },
+        ],
+      }
+      assert_equal(expected,
+                   { :request_queue   => @request_queue,
+                     :emitted_records => @emitted_records })
+    end
+  end
+
+  class PushEventTest < self
+    def test_multiple_commits
+      push_event = JSON.parse(fixture_data("push-event-multiple-commits.json"))
+      base = "https://api.github.com/repos/clear-code/fluent-plugin-github-activities/commits"
+      @crawler.process_push_event(push_event)
+      expected = {
+        :request_queue => [
+          { :type => ::Fluent::GithubActivities::TYPE_COMMIT,
+            :uri  => "#{base}/8e90721ff5d89f52b5b3adf0b86db01f03dc5588"},
+          { :type => ::Fluent::GithubActivities::TYPE_COMMIT,
+            :uri  => "#{base}/63e085b7607a3043cfbf9a866561807fbdda8a10"},
+          { :type => ::Fluent::GithubActivities::TYPE_COMMIT,
+            :uri  => "#{base}/c85e33bace040b7b42983e14d2b11a491d102072"},
+          { :type => ::Fluent::GithubActivities::TYPE_COMMIT,
+            :uri  => "#{base}/8ce6de7582376187e17e233dbae13575311a8c0b"},
+          { :type => ::Fluent::GithubActivities::TYPE_COMMIT,
+            :uri  => "#{base}/c908f319c7b6d5c5a69c8b675bde40dd990ee364"},
+        ],
+        :emitted_records => [
+          { :tag    => "push",
+            :record => push_event }
         ],
       }
       assert_equal(expected,
