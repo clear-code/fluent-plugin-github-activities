9b7dc56abc88d2b5e28a8a731e9e4e4769f89745
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-21 15:03:29 +0900

    Add test for commit comment event

--- test/test_crawler.rb
+++ test/test_crawler.rb
@@ -109,6 +109,23 @@ class CrawlerTest < Test::Unit::TestCase
     end
   end
 
+  class CommitCommentEventTest < self
+    def test_multiple_commits
+      event = JSON.parse(fixture_data("commit-comment-event.json"))
+      @crawler.process_user_event("user", event)
+      expected = {
+        :request_queue => [],
+        :emitted_records => [
+          { :tag    => "commit-comment",
+            :record => event }
+        ],
+      }
+      assert_equal(expected,
+                   { :request_queue   => @request_queue,
+                     :emitted_records => @emitted_records })
+    end
+  end
+
   class CommitTest < self
     def test_multiple_commits
       commit = JSON.parse(fixture_data("commit.json"))
