344799238d38aed4ab28a094f116fa47e793b4fc
Kenji Okimoto <okimoto@clear-code.com>
2017-02-22 15:48:41 +0900

    Indent

--- lib/fluent/plugin/in_github-activities.rb
+++ lib/fluent/plugin/in_github-activities.rb
@@ -24,90 +24,90 @@ require "fluent/plugin/github-activities"
 
 module Fluent
   module Plugin
-  class GithubActivitiesInput < Input
-    DEFAULT_BASE_TAG = "github-activity"
-    DEFAULT_CLIENTS = 4
+    class GithubActivitiesInput < Input
+      DEFAULT_BASE_TAG = "github-activity"
+      DEFAULT_CLIENTS = 4
 
-    Plugin.register_input("github-activities", self)
+      Plugin.register_input("github-activities", self)
 
-    config_param :access_token, :string, default: nil, secret: true
-    config_param :users, :string, default: nil
-    config_param :users_list, :string, default: nil
-    config_param :include_commits_from_pull_request, :bool, default: false
-    config_param :include_foreign_commits, :bool, default: false
-    config_param :base_tag, :string, default: DEFAULT_BASE_TAG
-    config_param :pos_file, :string, default: nil
-    config_param :clients, :integer, default: DEFAULT_CLIENTS
-    config_param :interval, :integer, default: 1
+      config_param :access_token, :string, default: nil, secret: true
+      config_param :users, :string, default: nil
+      config_param :users_list, :string, default: nil
+      config_param :include_commits_from_pull_request, :bool, default: false
+      config_param :include_foreign_commits, :bool, default: false
+      config_param :base_tag, :string, default: DEFAULT_BASE_TAG
+      config_param :pos_file, :string, default: nil
+      config_param :clients, :integer, default: DEFAULT_CLIENTS
+      config_param :interval, :integer, default: 1
 
-    def start
-      @base_tag = @base_tag.sub(/\.\z/, "")
+      def start
+        @base_tag = @base_tag.sub(/\.\z/, "")
 
-      users = prepare_users_list
-      n_clients = [@clients, users.size].min
-      @interval = @interval * n_clients
+        users = prepare_users_list
+        n_clients = [@clients, users.size].min
+        @interval = @interval * n_clients
 
-      @client_threads = []
-      @request_queue = Queue.new
+        @client_threads = []
+        @request_queue = Queue.new
 
-      users_manager_params = {
-        users: users,
-        pos_file: @pos_file,
-      }
-      users_manager = ::Fluent::GithubActivities::UsersManager.new(users_manager_params)
-      users_manager.generate_initial_requests.each do |request|
-        @request_queue.push(request)
-      end
+        users_manager_params = {
+          users: users,
+          pos_file: @pos_file,
+        }
+        users_manager = ::Fluent::GithubActivities::UsersManager.new(users_manager_params)
+        users_manager.generate_initial_requests.each do |request|
+          @request_queue.push(request)
+        end
 
-      n_clients.times do
-        @client_threads << Thread.new do
-          crawler_options = {
-            access_token: @access_token,
-            watching_users: users,
-            include_commits_from_pull_request: @include_commits_from_pull_request,
-            include_foreign_commits: @include_foreign_commits,
-            pos_file: @pos_file,
-            request_queue: @request_queue,
-            default_interval: @interval,
-          }
-          crawler = ::Fluent::GithubActivities::Crawler.new(crawler_options)
-          crawler.on_emit = lambda do |tag, record|
-            router.emit("#{@base_tag}.#{tag}", Engine.now, record)
-          end
+        n_clients.times do
+          @client_threads << Thread.new do
+            crawler_options = {
+              access_token: @access_token,
+              watching_users: users,
+              include_commits_from_pull_request: @include_commits_from_pull_request,
+              include_foreign_commits: @include_foreign_commits,
+              pos_file: @pos_file,
+              request_queue: @request_queue,
+              default_interval: @interval,
+            }
+            crawler = ::Fluent::GithubActivities::Crawler.new(crawler_options)
+            crawler.on_emit = lambda do |tag, record|
+              router.emit("#{@base_tag}.#{tag}", Engine.now, record)
+            end
 
-          loop do
-            crawler.process_request
-            sleep(crawler.interval_for_next_request)
+            loop do
+              crawler.process_request
+              sleep(crawler.interval_for_next_request)
+            end
           end
         end
       end
-    end
 
-    def shutdown
-      @client_threads.each(&:exit)
-    end
+      def shutdown
+        @client_threads.each(&:exit)
+      end
 
-    private
-    def prepare_users_list
-      @users ||= ""
-      users = @users.split(",")
+      private
+      def prepare_users_list
+        @users ||= ""
+        users = @users.split(",")
 
-      if @users_list
-        users_list = Pathname(@users_list)
-        if users_list.exist?
-          list = users_list.read
-          users += list.split("\n")
+        if @users_list
+          users_list = Pathname(@users_list)
+          if users_list.exist?
+            list = users_list.read
+            users += list.split("\n")
+          end
         end
-      end
 
-      users = users.collect do |user|
-        user.strip
-      end.reject do |user|
-        user.empty?
-      end
+        users = users.collect do |user|
+          user.strip
+        end.reject do |user|
+          user.empty?
+        end
 
-      users
+        users
+      end
     end
-  end
   end
 end
